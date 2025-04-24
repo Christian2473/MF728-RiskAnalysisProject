2331719c350aa76e127e0875d5be0845e3ea2e74
Christian2473 <135941125+Christian2473@users.noreply.github.com>
2025-04-24 14:38:28 -0400

    adding context manager to organize data

--- Data/CouponBondData/DataOrganization.py
+++ Data/CouponBondData/DataOrganization.py
@@ -1,5 +1,21 @@
 import pandas as pd
 import os
+from contextlib import contextmanager
+
+@contextmanager
+def open_folder(path: str):
+    """Context Manager that opens the folder and getting the path
+
+    Args:
+        path (str): Path to the folder
+    """
+    try:
+        cwd = os.getcwd()
+        os.chdir(path)
+        yield
+    finally:
+        os.chdir(cwd)
+    
 
 def organize_data(path: str) -> None:
     """Getting the rating of the bonds from an excel sheet path
@@ -22,17 +38,13 @@ def organize_data(path: str) -> None:
                 os.makedirs(rating)
 
             # Changing the Directory
-            os.chdir(rating)
+            with open_folder(rating):
+                file_path = os.path.join(os.getcwd(), sheet_name + ".csv")
 
-            file_path = os.path.join(os.getcwd(), sheet_name + ".csv")
-
-            # Checking if the file already exists
-            if not os.path.exists(file_path):
-                sheet_df = excel_file.parse(sheet_name)
-                sheet_df.to_csv(f"{sheet_name}.csv", index=False)
-
-            # Exiting Out of the Directory
-            os.chdir("..")
+                # Checking if the file already exists
+                if not os.path.exists(file_path):
+                    sheet_df = excel_file.parse(sheet_name)
+                    sheet_df.to_csv(f"{sheet_name}.csv", index=False)
 
 if __name__ == "__main__":
     os.chdir("Data/CouponBondData")
